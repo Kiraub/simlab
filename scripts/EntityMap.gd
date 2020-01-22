fb68cc1413de41fcac610463e60744d5a11f3cf7
Kiraub <kiraub.1997@gmail.com>
2020-01-22 16:32:41 +0100

    added Input captures to toggle various settings related to performance testing changed gui themeing added a largeMap scene with a larger map and more waiters added a function to automatically generate random new targets to simulate load

--- scripts/EntityMap.gd
+++ scripts/EntityMap.gd
@@ -11,22 +11,33 @@ enum E_Tiles {
 
 """ Variables """
 
+var rng = RandomNumberGenerator.new()
+export var provide_random_targets : bool = false
+export var deep_has_over_traversal : bool = true
+
 var entities : Array = [] setget , get_entities
 var actors : Array = []
 var statics : Array = []
-var highlighted_entites : Array = []
+var highlighted_entities : Array = []
+
+var static_free_tiles : Array = []
 var static_blocked_tiles : Array = []
+
+var cached_by_flag : Dictionary = {}
 
 """ Initialization """
 
 #[override]
 func _init():
 	z_index = GLOBALS.Z_INDICIES.BACKGROUND
+	rng.randomize()
+	var h = hash(rng.get_seed())
+	rng.set_seed(h)
 
 #[override]
 func _ready():
 	add_entities(collect_child_entities(self))
-	add_entities(collect_wall_entities(self))
+	add_entities(collect_static_entities(self))
 	center_entities()
 
 """ Simulation step """
@@ -34,12 +45,25 @@ func _ready():
 func step_entities(delta : float) -> void:
 	for actor in actors:
 		actor.step_time(delta)
-	#get_tree().call_group(GLOBALS.GROUP_NAMES.get(GLOBALS.GROUP_FLAGS.Acting), "step_time", delta)
+	if provide_random_targets:
+		for actor in actors:
+			if actor.position == actor.get_final_target():
+				highlighted_entities = [actor]
+				var random_target = get_random_free_tile()
+				handle_position_selection(random_target)
+				highlighted_entities.clear()
 
 """ Setters / Getters """
 
 func get_entities() -> Array:
 	return entities
+
+func get_random_free_tile() -> Vector2:
+	var random_tile = Vector2.ZERO
+	if len(static_free_tiles) > 0:
+		var random_index = rng.randi_range(0, len(static_free_tiles)-1)
+		random_tile = static_free_tiles[random_index]
+	return random_tile
 
 """ Static methods """
 
@@ -56,24 +80,31 @@ static func collect_child_entities(node : Node) -> Array:
 			array.append(subchild)
 	return array
 
-static func collect_wall_entities(entitymap : EntityMap) -> Array:
+static func collect_static_entities(entitymap : EntityMap) -> Array:
 	var array = []
 	for map in entitymap.get_used_cells():
 		var tile_index = entitymap.get_cellv(map)
+		var world = entitymap.map_to_world(map)
 		if tile_index == E_Tiles.WALL:
-			var world = entitymap.map_to_world(map)
 			var wall_entity = Static.new('Wall', world)
 			wall_entity.set_blocking(true)
 			entitymap.add_child(wall_entity)
 			array.push_back(wall_entity)
+		elif tile_index == E_Tiles.FLOOR:
+			var floor_entity = Static.new('Floor', world)
+			floor_entity.set_blocking(false)
+			entitymap.add_child(floor_entity)
+			array.push_back(floor_entity)
 	return array
 
 static func collect_entities_flags(entitymap : EntityMap, flags : int) -> Array:
-	var collection = []
-	for entity in entitymap.entities:
-		if (entity as Entity).EntityFlags & flags == flags:
-			collection.append(entity)
-	return collection
+	if not entitymap.cached_by_flag.has(flags):
+		var collection = []
+		for entity in entitymap.get_entities():
+			if (entity as Entity).EntityFlags & flags == flags:
+				collection.append(entity)
+		entitymap.cached_by_flag[flags] = collection
+	return entitymap.cached_by_flag.get(flags).duplicate()
 
 """ Methods """
 
@@ -85,6 +116,9 @@ func add_entity(entity : Entity) -> void:
 			if entity.is_blocking():
 				var tile = world_to_map(entity.position)
 				static_blocked_tiles.append(tile)
+			else:
+				var tile = world_to_map(entity.position)
+				static_free_tiles.append(tile)
 		elif entity is Actor:
 			actors.append(entity)
 func add_entities(new_entities : Array) -> void:
@@ -100,34 +134,36 @@ func center_entities() -> void:
 
 func handle_position_selection(map : Vector2) -> void:
 	var selectable_entities = collect_entities_flags(self, Entity.E_EntityFlags.Selectable)
-	print_debug(selectable_entities)
-	var clicked_entities = get_entities_at(map)
+	var clicked_entities = get_entities_at(map, Entity.E_EntityFlags.Selectable)
 	if len(clicked_entities) == 0:
-		print_debug("Empty space clicked")
-		for entity in highlighted_entites:
+		#print_debug("Empty space clicked")
+		for entity in highlighted_entities:
 			if entity is Actor:
-				var path = find_path((entity as Actor).get_final_target(), map)
-				if len(path) == 0:
-					print_debug("Empty or impossible path")
+				var actor_map = world_to_map((entity as Actor).get_final_target())
+				var path_map = find_path(actor_map, map)
+				if len(path_map) == 0:
+					#print_debug("Empty or impossible path")
 					continue
-				(entity as Actor).push_targets_back(path)
+				for point_map in path_map:
+					var point_world = center_to_cell(map_to_world(point_map))
+					(entity as Actor).push_target_back(point_world)
 				#(entity as Actor).push_target_back(map)
-				print_debug("Highlighted actor gets target")
+				#print_debug("Highlighted actor gets target")
 	else:
-		for entity in highlighted_entites:
+		for entity in highlighted_entities:
 			(entity as Entity).set_highlighted(false)
-		highlighted_entites.clear()
-		print_debug("Highlights cleared")
+		highlighted_entities.clear()
+		#print_debug("Highlights cleared")
 	for entity in clicked_entities:
 		if selectable_entities.has(entity):
 			(entity as Entity).set_highlighted(true)
-			highlighted_entites.append(entity)
-			print_debug("Entity highlighted")
+			highlighted_entities.append(entity)
+			#print_debug("Entity highlighted")
 
-func get_entities_at(map : Vector2) -> Array:
+func get_entities_at(map : Vector2, flags : int = 0) -> Array:
 	var target_entities = []
-	for entity in get_entities():
-		if center_to_cell(entity.position) == map:
+	for entity in collect_entities_flags(self, flags):
+		if world_to_map(entity.position) == map:
 			target_entities.push_back(entity)
 	return target_entities
 
@@ -137,8 +173,7 @@ func center_to_cell(world : Vector2) -> Vector2:
 	var center	:= origin + (cell_size / 2)
 	return center
 
-func tile_is_blocked(world : Vector2) -> bool:
-	var map = world_to_map(world)
+func tile_is_blocked(map : Vector2) -> bool:
 	if map in static_blocked_tiles:
 		return true
 	for actor in actors:
@@ -155,12 +190,12 @@ func find_path(map_start : Vector2, map_end : Vector2) -> Array:
 	return []
 
 func breadth_first_search(map_start : Vector2, map_end : Vector2) -> Array:
+	#print_debug(self, "Doing bfs from", map_start, "to", map_end)
 	var found_end = false
-	var expansions = [Vector2.UP * cell_size, Vector2.LEFT * cell_size, Vector2.DOWN * cell_size, Vector2.RIGHT * cell_size]
+	var expansions = [Vector2.UP, Vector2.LEFT, Vector2.DOWN, Vector2.RIGHT]
 	var paths = [[map_start]]
 	var blocked_tiles = []
 	var traversed_tiles = [map_start]
-	var DEEP_HAS_OVER_TRAVERSAL = true
 	while(len(paths) > 0 and not found_end):
 		var path : Array = paths.pop_front()
 		var last = path.back()
@@ -173,7 +208,7 @@ func breadth_first_search(map_start : Vector2, map_end : Vector2) -> Array:
 				continue
 			if path.has(new_point):
 				continue
-			if DEEP_HAS_OVER_TRAVERSAL:
+			if deep_has_over_traversal:
 				if deep_has(paths, new_point):
 					continue
 			elif traversed_tiles.has(new_point):
--- scripts/Simulation.gd
+++ scripts/Simulation.gd
@@ -8,18 +8,36 @@ class_name Simulation
 
 export var simulation_speed : float = 1.0
 export var highlighted : bool = false setget set_highlighted, is_highlighted
+
+export var use_physics_process : bool = true
+
 var step_count : float = 0.0
+var viewport_container : ViewportContainer
 var entity_map : EntityMap
 
 """ Initialization """
 
 func _ready() -> void:
+	if bind_viewport_container(self):
+		viewport_container.connect("gui_input", self, "_gui_input", [])
+		print_debug("ViewportContainer bound to simulation.")
+	else:
+		print_debug("ViewportContainer not found underneath simulation.")
+		get_tree().quit()
 	if bind_entity_map(self):
 		print_debug("EntityMap bound to simulation.")
 	else:
 		print_debug("EntityMap not found underneath simulation.")
+		get_tree().quit()
+	toggle_processing()
 
 """ Godot process """
+
+func _process(delta):
+	step_count += simulation_speed
+	while step_count >= 1.0:
+		step_count -= 1.0
+		entity_map.step_entities(delta)
 
 func _physics_process(delta):
 	step_count += simulation_speed
@@ -31,14 +49,33 @@ func _physics_process(delta):
 
 func set_highlighted(new_highlighted : bool) -> void:
 	if new_highlighted:
-		
+		pass
+func is_highlighted()-> bool:
+	return highlighted
 
 """ Methods """
+
+func toggle_processing() -> void:
+	if use_physics_process:
+		set_process(false)
+		set_physics_process(true)
+	else:
+		set_process(true)
+		set_physics_process(false)
 
 func bind_entity_map(from : Node) -> bool:
 	for child in from.get_children():
 		if child is EntityMap:
 			entity_map = child
+			return true
+		elif bind_entity_map(child):
+			return true
+	return false
+
+func bind_viewport_container(from : Node) -> bool:
+	for child in from.get_children():
+		if child is ViewportContainer:
+			viewport_container = child
 			return true
 		elif bind_entity_map(child):
 			return true
@@ -52,13 +89,19 @@ func _unhandled_input(event : InputEvent) -> void:
 		simulation_speed *= 2
 	if Input.is_action_just_pressed("ui_down"):
 		simulation_speed /= 2
+	if Input.is_action_just_pressed("ui_accept"):
+		use_physics_process = not use_physics_process
+		toggle_processing()
+	if Input.is_action_just_pressed("ui_select"):
+		entity_map.provide_random_targets = not entity_map.provide_random_targets
+	if Input.is_action_just_pressed("ui_focus_next"):
+		entity_map.deep_has_over_traversal = not entity_map.deep_has_over_traversal
 
 func _gui_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
-			print_debug(self, " has received a gui lmb click")
 			handle_lmb_click(event)
 
 func handle_lmb_click(event : InputEventMouseButton) -> void:
-	var map_position = entity_map.center_to_cell(event.position)
+	var map_position = entity_map.world_to_map(event.position)
 	entity_map.handle_position_selection(map_position)
