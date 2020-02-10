8d6e2d10747d06e23fd0a1057ed8865d8e770a06
Kiraub <kiraub.1997@gmail.com>
2020-02-10 17:40:30 +0100

    Simulation: added step_delay that determines how many milliseconds the simulation waits to take the next step

--- scripts/Simulation.gd
+++ scripts/Simulation.gd
@@ -14,7 +14,9 @@ var paused			: bool		= true			setget set_paused
 var partial_steps	: bool		= false			setget set_partial_steps
 var random_targets	: bool		= false			setget set_random_targets
 var highlighted		: bool		= false			#setget set_highlighted
-var process_speed	: float		= 1.0			setget set_process_speed
+var step_delay		: float		= 1_000.0		setget set_step_delay
+var step_size		: float		= 1.0			setget set_step_size
+var _delay_acc		: float		= 0.0
 var _step_acc		: float		= 0.0
 
 var config			: ConfigWrapper				setget , get_config_wrapper
@@ -39,43 +41,66 @@ func _ready() -> void:
 	
 	config = ConfigWrapper.new("Simulation")
 	config.add_config_entry("title", {
-		ConfigWrapper.CONFIG_FIELDS[0]: "Title",
-		ConfigWrapper.CONFIG_FIELDS[1]: title,
-		ConfigWrapper.CONFIG_FIELDS[2]: "title_changed"
+		ConfigWrapper.FIELDS.LABEL_TEXT: "Title",
+		ConfigWrapper.FIELDS.DEFAULT_VALUE: title,
+		ConfigWrapper.FIELDS.SIGNAL_NAME: "title_changed"
 	})
 	config.add_config_entry("partial_steps", {
-		ConfigWrapper.CONFIG_FIELDS[0]: "Partial steps",
-		ConfigWrapper.CONFIG_FIELDS[1]: partial_steps,
-		ConfigWrapper.CONFIG_FIELDS[2]: "partial_steps_changed"
+		ConfigWrapper.FIELDS.LABEL_TEXT: "Partial steps",
+		ConfigWrapper.FIELDS.DEFAULT_VALUE: partial_steps,
+		ConfigWrapper.FIELDS.SIGNAL_NAME: "partial_steps_changed"
 	})
 	config.add_config_entry("random_targets" , {
-		ConfigWrapper.CONFIG_FIELDS[0]: "Random targets",
-		ConfigWrapper.CONFIG_FIELDS[1]: random_targets,
-		ConfigWrapper.CONFIG_FIELDS[2]: "random_targets_changed"
+		ConfigWrapper.FIELDS.LABEL_TEXT: "Random targets",
+		ConfigWrapper.FIELDS.DEFAULT_VALUE: random_targets,
+		ConfigWrapper.FIELDS.SIGNAL_NAME: "random_targets_changed"
 	})
-	config.add_config_entry("process_speed", {
-		ConfigWrapper.CONFIG_FIELDS[0]: "Process Speed",
-		ConfigWrapper.CONFIG_FIELDS[1]: process_speed,
-		ConfigWrapper.CONFIG_FIELDS[2]: "process_speed_changed"
+	config.add_config_entry("step_delay", {
+		ConfigWrapper.FIELDS.LABEL_TEXT: "Step delay (ms)",
+		ConfigWrapper.FIELDS.DEFAULT_VALUE: step_delay,
+		ConfigWrapper.FIELDS.SIGNAL_NAME: "step_delay_changed"
+	})
+	config.add_config_entry("step_size", {
+		ConfigWrapper.FIELDS.LABEL_TEXT: "Step size",
+		ConfigWrapper.FIELDS.DEFAULT_VALUE: step_size,
+		ConfigWrapper.FIELDS.SIGNAL_NAME: "step_size_changed"
 	})
 	config.add_config_entry("entity_map",  {
-		ConfigWrapper.CONFIG_FIELDS[0]: "Entity Map",
-		ConfigWrapper.CONFIG_FIELDS[1]: entity_map,
-		ConfigWrapper.CONFIG_FIELDS[2]: "entity_map_changed"
+		ConfigWrapper.FIELDS.LABEL_TEXT: "Entity Map",
+		ConfigWrapper.FIELDS.DEFAULT_VALUE: entity_map,
+		ConfigWrapper.FIELDS.SIGNAL_NAME: "entity_map_changed"
 	})
 	config.connect("title_changed", self, "_on_title_changed")
 	config.connect("partial_steps_changed", self, "_on_partial_steps_changed")
 	config.connect("random_targets_changed", self, "_on_random_targets_changed")
-	config.connect("process_speed_changed", self, "_on_process_speed_changed")
+	config.connect("step_delay_changed", self, "_on_step_delay_changed")
+	config.connect("step_size_changed", self, "_on_step_size_changed")
 	config.connect("entity_map_changed", self, "_on_entity_map_changed")
-	
+
+""" Simulation step """
+
+func step_by(amount : float = 1.0) -> void:
+	var steps_taken = 0.0
+	_step_acc += amount
+	if partial_steps:
+		steps_taken = _step_acc
+		_step_acc = 0.0
+	elif _step_acc >= 1.0:
+		steps_taken = floor(_step_acc)
+		_step_acc -= steps_taken
+	if steps_taken > 0.0:
+		entity_map.step_by(steps_taken)
+		emit_signal("simulation_stepped", steps_taken)
 
 """ Godot process """
 
-func _process(_delta : float) -> void:
+func _process(delta_in_seconds : float) -> void:
 	if paused:
 		return
-	step_by(process_speed)
+	_delay_acc += delta_in_seconds * 1_000
+	if _delay_acc > step_delay:
+		_delay_acc -= step_delay
+		step_by(step_size)
 
 """ Setters / Getters """
 
@@ -97,56 +122,35 @@ func set_random_targets(new_value : bool) -> void:
 	config.set_entry_value("random_targets", random_targets)
 	entity_map.provide_random_targets = random_targets
 
-func set_process_speed(new_value : float) -> void:
-	process_speed = new_value
-	config.set_entry_value("process_speed", process_speed)
+func set_step_delay(new_value : float) -> void:
+	step_delay = new_value
+	config.set_entry_value("step_delay", step_delay)
+
+func set_step_size(new_value : float) -> void:
+	step_size = new_value
+	config.set_entry_value("step_size", step_size)
 
 func get_config_wrapper() -> ConfigWrapper:
 	return config
 
 """ Methods """
 
-func step_by(amount : float = 1.0) -> void:
-	var steps_taken = 0.0
-	_step_acc += amount
-	if partial_steps:
-		steps_taken = _step_acc
-		_step_acc = 0.0
-	elif _step_acc >= 1.0:
-		steps_taken = floor(_step_acc)
-		_step_acc -= steps_taken
-	if steps_taken > 0.0:
-		entity_map.step_by(steps_taken)
-		emit_signal("simulation_stepped", steps_taken)
-
 func handle_lmb_click(event : InputEventMouseButton) -> void:
 	if not entity_map is EntityMap:
 		return
 	var map_position = entity_map.world_to_map(event.position)
 	entity_map.handle_position_selection(map_position)
-
-func double_process_speed() -> void:
-	set_process_speed(process_speed * 2)
-
-func half_process_speed() -> void:
-	set_process_speed(process_speed / 2)
+	get_tree().set_input_as_handled()
 
 """ Events """
 
 func _unhandled_input(event : InputEvent) -> void:
-	# Events for Simulation
-	if Input.is_action_just_pressed("ui_up"):
-		double_process_speed()
-	if Input.is_action_just_pressed("ui_down"):
-		half_process_speed()
 	# Events for EntityMap
 	if not entity_map is EntityMap:
 		return
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
 			handle_lmb_click(event)
-	if Input.is_action_just_pressed("ui_focus_next"):
-		entity_map.deep_has_over_traversal = not entity_map.deep_has_over_traversal
 
 func _gui_input(event : InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -162,8 +166,11 @@ func _on_partial_steps_changed(_old_value : bool, new_value : bool) -> void:
 func _on_random_targets_changed(_old_value : bool, new_value : bool) -> void:
 	set_random_targets(new_value)
 
-func _on_process_speed_changed(_old_value : float, new_value : float) -> void:
-	set_process_speed(new_value)
+func _on_step_delay_changed(_old_value : float, new_value : float) -> void:
+	set_step_delay(new_value)
+
+func _on_step_size_changed(_old_value : float, new_value : float) -> void:
+	set_step_size(new_value)
 
 func _on_entity_map_changed() -> void:
 	pass
@@ -174,9 +181,9 @@ func _on_ConfigBtn_pressed() -> void:
 func _on_PlayPauseBtn_pressed(play : bool) -> void:
 	set_paused(!play)
 
-func _on_StepBtn_pressed():
+func _on_StepBtn_pressed(amount : float = step_size):
 	if paused:
-		step_by()
+		step_by(amount)
 
 func _on_DeleteBtn_pressed():
 	self.queue_free()
